8e254b4df32c2f155ffab272828a44094cf26ba3
ElenaGeorgievska <elena_science@yahoo.com>
2023-08-27 03:03:16 +0200

    stage1_module6_io_task1 completed

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -17,7 +17,7 @@ public class FileReader {
     Map<String, String> result = new HashMap<>();
     String[] array = {};
 
-    public Profile getDataFromFile(File file) {       //String file = "src/test/resources/Profile.txt";
+    public Profile getDataFromFile(File file) {
 
         try (BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(file)))) {
             while ((line = reader.readLine()) != null) {
@@ -32,10 +32,10 @@ public class FileReader {
         return new Profile(result.get("Name").trim(), Integer.parseInt(result.get("Age").trim()), result.get("Email").trim(), Long.parseLong(result.get("Phone").trim()));
     }
 
-    public static void main(String[] args) {
-        FileReader fileReader = new FileReader();
-        System.out.println(fileReader.getDataFromFile(new File("src/main/resources/Profile.txt")));
-    }
+//    public static void main(String[] args) {
+//        FileReader fileReader = new FileReader();
+//        System.out.println(fileReader.getDataFromFile(new File("src/main/resources/Profile.txt")));
+//    }
 
     //https://mkyong.com/java/how-to-read-file-in-java-fileinputstream/
 }
